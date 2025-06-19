64bcd5b6e4f545352b5c3b45350690c14a604e9d
Bruno Sabença <bruno@brunosabenca.com>
2025-06-19 22:34:19 +0100

    Optimise storage and automatic gc

--- modules/core/nix/default.nix
+++ modules/core/nix/default.nix
@@ -17,4 +17,18 @@
     home.homeDirectory = "/home/${username}";
     home.stateVersion = "23.05";
   };
+
+  # Perform automatic garbage collection of the nix store
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
 }
