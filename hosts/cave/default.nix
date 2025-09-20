ac224f87cc9a249449e79c3754da8a0d48c93069
Bruno Sabença <bruno@brunosabenca.com>
2025-09-20 16:19:38 +0100

    Fix cave config

--- hosts/cave/default.nix
+++ hosts/cave/default.nix
@@ -91,79 +91,79 @@
         X11Forwarding = false;
         PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
       };
+    };
 
-      keyd = {
-        enable = true;
-        keyboards = {
-          default = {
-            ids = [ "*" ];
-            extraConfig = ''
-              [main]
-              f1 = back
-              f2 = forward
-              f3 = refresh
-              f4 = f11
-              f5 = scale
-              f6 = brightnessdown
-              f7 = brightnessup
-              f8 = mute
-              f9 = volumedown
-              f10 = volumeup
+    keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "*" ];
+          extraConfig = ''
+            [main]
+            f1 = back
+            f2 = forward
+            f3 = refresh
+            f4 = f11
+            f5 = scale
+            f6 = brightnessdown
+            f7 = brightnessup
+            f8 = mute
+            f9 = volumedown
+            f10 = volumeup
 
-              back = back
-              forward = forward
-              refresh = refresh
-              zoom = f11
-              scale = scale
-              brightnessdown = brightnessdown
-              brightnessup = brightnessup
-              mute = mute
-              volumedown = volumedown
-              volumeup = volumeup
+            back = back
+            forward = forward
+            refresh = refresh
+            zoom = f11
+            scale = scale
+            brightnessdown = brightnessdown
+            brightnessup = brightnessup
+            mute = mute
+            volumedown = volumedown
+            volumeup = volumeup
 
-              f13=coffee
-              sleep=coffee
+            f13=coffee
+            sleep=coffee
 
-              [meta]
-              f1 = f1
-              f2 = f2
-              f3 = f3
-              f4 = f4
-              f5 = f5
-              f6 = f6
-              f7 = f7
-              f8 = f8
-              f9 = f9
-              f10 = f10
+            [meta]
+            f1 = f1
+            f2 = f2
+            f3 = f3
+            f4 = f4
+            f5 = f5
+            f6 = f6
+            f7 = f7
+            f8 = f8
+            f9 = f9
+            f10 = f10
 
-              back = f1
-              forward = f2
-              refresh = f3
-              zoom = f4
-              scale = f5
-              brightnessdown = f6
-              brightnessup = f7
-              mute = f8
-              volumedown = f9
-              volumeup = f10
+            back = f1
+            forward = f2
+            refresh = f3
+            zoom = f4
+            scale = f5
+            brightnessdown = f6
+            brightnessup = f7
+            mute = f8
+            volumedown = f9
+            volumeup = f10
 
 
-              [alt]
-              backspace = delete
-              brightnessdown = kbdillumdown
-              brightnessup = kbdillumup
-              f6 = kbdillumdown
-              f7 = kbdillumup
+            [alt]
+            backspace = delete
+            brightnessdown = kbdillumdown
+            brightnessup = kbdillumup
+            f6 = kbdillumdown
+            f7 = kbdillumup
 
-              [control]
-              f5 = sysrq
-              scale = sysrq
+            [control]
+            f5 = sysrq
+            scale = sysrq
 
-              [control+alt]
-              backspace = C-A-delete
+            [control+alt]
+            backspace = C-A-delete
 
-            '';
-          };
+          '';
         };
       };
     };
