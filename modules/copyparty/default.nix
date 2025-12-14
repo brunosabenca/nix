6d93e687466c67430b0c6721eda689fa60a81a75
Bruno Sabença <bruno@brunosabenca.com>
2025-12-14 23:01:12 +0000

    No service

--- modules/copyparty/default.nix
+++ modules/copyparty/default.nix
@@ -9,61 +9,62 @@
   home-manager.users.${username} =
     {
       age,
+      config,
       ...
     }:
     {
       age.secrets."copyparty.bruno".file = ./copyparty.bruno.age;
 
-      services.copyparty = {
-        enable = true;
-        # directly maps to values in the [global] section of the copyparty config.
-        # see `copyparty --help` for available options
-        settings = {
-          i = "0.0.0.0";
-          # use lists to set multiple values
-          p = [
-            3923
-          ];
-          # use booleans to set binary flags
-          no-reload = true;
-          # using 'false' will do nothing and omit the value when generating a config
-          ignored-flag = false;
-        };
-
-        accounts = {
-          bruno.passwordFile = config.age.secrets."copyparty.bruno".path;
-        };
-
-        volumes = {
-          "/" = {
-            path = "/mnt/data";
-            # see `copyparty --help-accounts` for available options
-            access = {
-              # users "ed" and "k" get read-write
-              rw = [
-                "bruno"
-              ];
-            };
-            # see `copyparty --help-flags` for available options
-            flags = {
-              # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-              fk = 4;
-              # scan for new files every 60sec
-              scan = 60;
-              # volflag "e2d" enables the uploads database
-              e2d = true;
-              # "d2t" disables multimedia parsers (in case the uploads are malicious)
-              d2t = true;
-              # skips hashing file contents if path matches *.iso
-              nohash = "\.iso$";
-            };
-          };
-        };
-        # you may increase the open file limit for the process
-        openFilesLimit = 8192;
-      };
-
     };
+
+  services.copyparty = {
+    enable = true;
+    # directly maps to values in the [global] section of the copyparty config.
+    # see `copyparty --help` for available options
+    settings = {
+      i = "0.0.0.0";
+      # use lists to set multiple values
+      p = [
+        3923
+      ];
+      # use booleans to set binary flags
+      no-reload = true;
+      # using 'false' will do nothing and omit the value when generating a config
+      ignored-flag = false;
+    };
+
+    accounts = {
+      bruno.passwordFile = config.age.secrets."copyparty.bruno".path;
+    };
+
+    volumes = {
+      "/" = {
+        path = "/mnt/data";
+        # see `copyparty --help-accounts` for available options
+        access = {
+          # users "ed" and "k" get read-write
+          rw = [
+            "bruno"
+          ];
+        };
+        # see `copyparty --help-flags` for available options
+        flags = {
+          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
+          fk = 4;
+          # scan for new files every 60sec
+          scan = 60;
+          # volflag "e2d" enables the uploads database
+          e2d = true;
+          # "d2t" disables multimedia parsers (in case the uploads are malicious)
+          d2t = true;
+          # skips hashing file contents if path matches *.iso
+          nohash = "\.iso$";
+        };
+      };
+    };
+    # you may increase the open file limit for the process
+    openFilesLimit = 8192;
+  };
 
   age.secrets."copyparty.acme".file = ./copyparty.acme.age;
 
