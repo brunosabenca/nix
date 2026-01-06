a98e561a98480a94a729008abff870c73f0d9a2b
Bruno Sabença <bruno@brunosabenca.com>
2026-01-06 22:46:57 +0000

    Mount cave on firefly

--- hosts/firefly/default.nix
+++ hosts/firefly/default.nix
@@ -12,6 +12,27 @@
   ];
 
   home-manager.users.${username} = {pkgs, ...}: {
+      xdg.configFile."rclone/rclone-nixos.conf".text = ''
+        [cave]
+        type = sftp
+        host = 192.168.1.30
+        user = bruno
+        key_file = ${config.users.users.bruno.home}/.ssh/id_ed25519
+      '';
+
+      systemd.user.services.mount-cave = {
+        Unit = {
+          Description = "Mount cave with rclone";
+          After = [ "network-online.target" ];
+        };
+        Service = {
+          ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${config.users.users.bruno.home}/cave";
+          ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/rclone/rclone-nixos.conf --vfs-cache-mode writes --ignore-checksum mount \"cave:/mnt/data\" \"cave\"";
+          ExecStop = "/run/current-system/sw/bin/fusermount -u %h/cave/%i";
+          Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+        };
+        Install.WantedBy = [ "default.target" ];
+      };
   };
 
   networking = {
