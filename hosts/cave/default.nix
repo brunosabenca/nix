0544084dbca3e9b971a9938a07fd2148109b1b74
Bruno Sabença <bruno@brunosabenca.com>
2026-01-30 12:11:19 +0000

    Add tlp

--- hosts/cave/default.nix
+++ hosts/cave/default.nix
@@ -9,10 +9,37 @@
     ./audio-configuration.nix
   ];
 
+virtualisation.podman = {
+  enable = true;
+  dockerCompat = true;
+};
+
+
+services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+       START_CHARGE_THRESH_BAT0 = 40;
+       STOP_CHARGE_THRESH_BAT0 = 80;
+
+      };
+};
+
   nixpkgs.overlays = [ copyparty.overlays.default ];
   environment.systemPackages = [
     pkgs.copyparty
     pkgs.cfssl
+    pkgs.distrobox
   ];
   services.tailscale.enable = true;
 
