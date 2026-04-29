2256c625fec66067e4510e1fcaa6c82d907def17
Bruno Sabença <bruno@brunosabenca.com>
2026-04-30 00:33:08 +0100

    fix: migrate WirePlumber Bluetooth config to 0.5 format
    WirePlumber 0.5 dropped Lua config support, silently ignoring the old
    bluetooth.lua.d config and breaking A2DP sink profile negotiation.

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -88,12 +88,12 @@
       ];
 
       home.file = {
-        ".config/wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-          bluez_monitor.properties = {
-          	["bluez5.enable-sbc-xq"] = true,
-          	["bluez5.enable-msbc"] = true,
-          	["bluez5.enable-hw-volume"] = true,
-          	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        ".config/wireplumber/wireplumber.conf.d/51-bluez-config.conf".text = ''
+          monitor.bluez.properties = {
+            bluez5.enable-sbc-xq = true
+            bluez5.enable-msbc = true
+            bluez5.enable-hw-volume = true
+            bluez5.headset-roles = [ hsp_hs hsp_ag hfp_hf hfp_ag ]
           }
         '';
       };
