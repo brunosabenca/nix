f4a0d6003e41c8e9470102618e221d1c995fd981
Bruno Sabença <bruno@brunosabenca.com>
2026-05-07 20:07:13 +0100

    Configure battery thresholds for firefly

--- hosts/firefly/default.nix
+++ hosts/firefly/default.nix
@@ -18,14 +18,21 @@
     spotify
   ];
 
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="power_supply", KERNEL=="BAT0", ATTR{charge_control_start_threshold}="40", ATTR{charge_control_end_threshold}="80"
+  '';
+
   home-manager.users.${username}.programs.fish.functions = {
     battery-full = {
       description = "Temporarily charge battery to 100%";
       body = "echo 100 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold > /dev/null";
     };
     battery-save = {
-      description = "Restore battery charge limit to 80%";
-      body = "echo 80 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold > /dev/null";
+      description = "Restore battery charge thresholds to 40-80%";
+      body = ''
+        echo 40 | sudo tee /sys/class/power_supply/BAT0/charge_control_start_threshold > /dev/null
+        echo 80 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold > /dev/null
+      '';
     };
   };
 }
