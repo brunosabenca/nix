a720781834c1a91fa012255b90503e496585298f
Bruno Sabença <bruno@brunosabenca.com>
2026-05-07 20:25:02 +0100

    feat(firefly): auto-restore battery thresholds after full charge
    Adds a systemd oneshot service triggered by udev when battery status
    reaches Full, restoring custom 50-80% thresholds automatically after
    a battery-full cycle.

--- hosts/firefly/default.nix
+++ hosts/firefly/default.nix
@@ -19,6 +19,18 @@
     spotify
   ];
 
+  systemd.services.battery-restore-thresholds = {
+    description = "Restore battery charge thresholds to custom 50-80% after full charge";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.libsmbios}/bin/smbios-battery-ctl --set-charging-mode=custom --set-custom-charge-interval=50 80";
+    };
+  };
+
+  services.udev.extraRules = ''
+    ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="BAT0", ATTR{status}=="Full", TAG+="systemd", ENV{SYSTEMD_WANTS}="battery-restore-thresholds.service"
+  '';
+
   home-manager.users.${username}.programs.fish.functions = {
     battery-full = {
       description = "Temporarily charge battery to 100%";
